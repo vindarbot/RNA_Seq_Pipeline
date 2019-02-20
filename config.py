03669e2b4a317127fdc4a9e2a37a86b9579f1b96
Vincent Darbot <41127566+Darbinator@users.noreply.github.com>
2019-02-20 17:09:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,26 @@
 ##################################################
 
 
+### General
+
+READ_LENGHT = 	150
+
+GET_GENOME	=	"https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_chromosome_files/TAIR10_chr_all.fas"
+
+GET_GTF		=	"https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff"
+
+GET_DESCRIPTION = "https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/gene_description_20131231.txt.gz"
+
 ### Trimming
+
 TRIMMING =		True
+
+# path to adapters
 ADAPTERS =		"/Users/vindarbo/happy_bin/bbmap/resources/adapters.fa"
+
+# minimum length of reads to keep after trimming
 minlen   =		25 
+
 ktrim    =		"r" 
 k        =		22 
 qtrim    =		"rl" 
@@ -20,3 +36,9 @@ hdist    =		1
 
 
 ### Mapping
+
+# path to reference genome (default = TAIR10.fasta)
+GENOME	=		'Reference/TAIR10.fasta'
+
+# path to gtf file with transcripts (default = TAIR10.gtf)
+GTF		=		'TAIR10.gtf'
