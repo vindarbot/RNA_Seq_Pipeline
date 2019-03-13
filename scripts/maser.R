ef431efdb87bebc85f08ed125de5348f9c3424bb
Vincent Darbot <darbotvincent@gmail.com>
2019-03-13 11:19:50 +0100

    blabla

--- scripts/maser.R
+++ scripts/maser.R
@@ -1,7 +1,10 @@
 library(ggplot2)
 library(tidyverse)
 library(maser)
+library(biomaRt)
+### QC des données géénréas par rMATS
 
+?maser()
 dir = setwd("~/Desktop/Data")
 
 
@@ -14,4 +17,23 @@ path <- "rMATS_hon4HS/"
 hon4 <- maser(path, c("ColHS","hon4_HS"), ftype="JCEC")
 
 head(summary(hon4, type = "SE")[, 1:8])
+
+hon4_filt <- filterByCoverage(hon4,avg_reads = 5)
+
+hon4_top <- topEvents(hon4_filt, fdr = 0.05, deltaPSI = 0.1)
+
+ensembl <- useMart(biomart="plants_mart",host="plants.ensembl.org",dataset = "athaliana_eg_gene")
+symbol <- getBM(attributes=c("tair_symbol","tair_locus"), mart=ensembl)
+
+merge <- merge(x=symbol,y=annotation(hon4_top),by.x="tair_locus",by.y="GeneID",sort=F,all.y=T)
+
+
+table_hon4 <- annotation(hon4_top)
+
+
+volcano(hon4,fdr=0.05,deltaPSI=0.1)
+
+events <- geneEvents(hon4_filt,fdr=0.05,deltaPSI=0.1)
+
+plotTranscripts(hon4,type="SE",gtf="Reference")
 
